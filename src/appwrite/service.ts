0b2025f74e370377384b5521d528c0f6576917a5
darshil929 <darshilt929@gmail.com>
2024-03-22 10:47:46 +0530

    configurations

---
+++ src/appwrite/service.ts
@@ -0,0 +1,20 @@
+import { ID, Account, Client } from "appwrite";
+import Config from "react-native-config";
+
+import Snackbar from "react-native-snackbar";
+
+const appWriteClient = new Client()
+
+const APPWRITE_ENDPOINT: string = Config.APPWRITE_ENDPOINT!;
+const APPWRITE_PROJECT_ID: string = Config.APPWRITE_PROJECT_ID!;
+
+type CreateUserAccount = {
+    email: string;
+    password: string;
+    name: string;
+}
+
+type LoginUserAccount = {
+    email: string;
+    password: string;
+}
